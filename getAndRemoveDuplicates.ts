3b1df0300debd3e63069bd6ea47203defac24f78
Steve Thompson <writetome51@gmail.com>
2018-10-13 02:18:02 -0600

    initial commit

---
+++ getAndRemoveDuplicates.ts
@@ -0,0 +1,12 @@
+import { getDuplicates } from '@writetome51/array-get-duplicates/getDuplicates';
+import { removeFirstOfEach } 
+	from '@writetome51/array-remove-all-of-first-of/removeFirstOf_removeFirstOfEach';
+
+// Returns and removes every extra instance of each array item.
+// Remaining items in array may be out of order.
+
+export function getAndRemoveDuplicates(array): any[] {
+	let duplicates = getDuplicates(array);
+	removeFirstOfEach(duplicates, array);
+	return duplicates;
+}
