8251897905c6de2419ef9e12fe5d4fb44c5ae1a6
Steve Thompson <writetome51@gmail.com>
2018-10-21 22:31:27 -0600

    minor change

--- lib/tests.ts
+++ lib/tests.ts
@@ -1,5 +1,5 @@
 import { getAndRemoveDuplicates } from './getAndRemoveDuplicates';
-import { arraysMatch } from '@writetome51/arrays-match/arraysMatch';
+import { arraysMatch } from '@writetome51/arrays-match';
 
 
 let arr: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 1, 2, 3];
